fb378e3acd6b0dc45d2777af9747efc80a82a82d
Rainer Bieniek <Rainer.Bieniek@web.de>
2013-09-23 02:13:42 +0200

    added gitignore

---
+++ objective-c/APIX-OAuth2-Lib/APIX-OAuth2-LibTests/APIX_OAuth2_LibTests.h
@@ -0,0 +1,13 @@
+//
+//  APIX_OAuth2_LibTests.h
+//  APIX-OAuth2-LibTests
+//
+//  Created by Rainer Bieniek on 23.09.13.
+//  Copyright (c) 2013 Vodafone Group Service, Product & Services. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface APIX_OAuth2_LibTests : SenTestCase
+
+@end
---
+++ objective-c/APIX-OAuth2-Lib/APIX-OAuth2-LibTests/APIX_OAuth2_LibTests.m
@@ -0,0 +1,32 @@
+//
+//  APIX_OAuth2_LibTests.m
+//  APIX-OAuth2-LibTests
+//
+//  Created by Rainer Bieniek on 23.09.13.
+//  Copyright (c) 2013 Vodafone Group Service, Product & Services. All rights reserved.
+//
+
+#import "APIX_OAuth2_LibTests.h"
+
+@implementation APIX_OAuth2_LibTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in APIX-OAuth2-LibTests");
+}
+
+@end
