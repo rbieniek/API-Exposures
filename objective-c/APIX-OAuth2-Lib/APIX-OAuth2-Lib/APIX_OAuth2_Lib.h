fe2b287f91a57017fbb2909cd1513629c7ec17df
Rainer Bieniek <Rainer.Bieniek@web.de>
2013-09-23 02:06:18 +0200

    Baseline checkin

---
+++ objective-c/APIX-OAuth2-Lib/APIX-OAuth2-Lib/APIX_OAuth2_Lib.h
@@ -0,0 +1,13 @@
+//
+//  APIX_OAuth2_Lib.h
+//  APIX-OAuth2-Lib
+//
+//  Created by Rainer Bieniek on 23.09.13.
+//  Copyright (c) 2013 Vodafone Group Service, Product & Services. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APIX_OAuth2_Lib : NSObject
+
+@end
---
+++ objective-c/APIX-OAuth2-Lib/APIX-OAuth2-Lib/APIX_OAuth2_Lib.m
@@ -0,0 +1,13 @@
+//
+//  APIX_OAuth2_Lib.m
+//  APIX-OAuth2-Lib
+//
+//  Created by Rainer Bieniek on 23.09.13.
+//  Copyright (c) 2013 Vodafone Group Service, Product & Services. All rights reserved.
+//
+
+#import "APIX_OAuth2_Lib.h"
+
+@implementation APIX_OAuth2_Lib
+
+@end
