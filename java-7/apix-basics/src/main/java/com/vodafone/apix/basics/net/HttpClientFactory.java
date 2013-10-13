09cb36574d0e4771473a22174d2c466e3e7e22c3
Rainer Bieniek <Rainer.Bieniek@web.de>
2013-10-13 03:42:08 +0200

    - work on HTTP client factory

--- java-7/apix-basics/src/main/java/com/vodafone/apix/basics/net/HttpClientFactory.java
+++ java-7/apix-basics/src/main/java/com/vodafone/apix/basics/net/HttpClientFactory.java
@@ -3,6 +3,19 @@
  */
 package com.vodafone.apix.basics.net;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ServiceLoader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vodafone.apix.basics.ApixRuntimeException;
+import com.vodafone.apix.basics.net.impl.HttpClientImpl;
+import com.vodafone.apix.basics.net.spi.IHttpClientSpi;
+import com.vodafone.apix.basics.net.spi.IHttpClientSpiProvider;
+
 /**
  * Factory for generating HTTP client instances.  
  * 
@@ -10,18 +23,64 @@ package com.vodafone.apix.basics.net;
  *
  */
 public class HttpClientFactory {
+	private static final Logger logger = LoggerFactory.getLogger(HttpClientFactory.class);
 
+	private List<IHttpClientSpiProvider> providers = new LinkedList<>();
 
 	/**
 	 * 
 	 */
 	private HttpClientFactory() {
+		try {
+			Iterator<IHttpClientSpiProvider> loaderIt = ServiceLoader.load(IHttpClientSpiProvider.class).iterator();
+			
+			while(loaderIt.hasNext())
+				providers.add(loaderIt.next());
+		} catch(Exception e) {
+			logger.error("failed to initialize ", e);
+		}
 	}
 
-	private static HttpClientFactory _instance = new HttpClientFactory();
+	private static HttpClientFactory _instance = null;
 	
+	/**
+	 * Obtain the singular instance. The instance creation is delayed until the instance is requested.
+	 * 
+	 * @return
+	 */
 	public static final HttpClientFactory instance() {
+		if(_instance == null)
+			_instance = new HttpClientFactory();
+		
 		return _instance;
+	}
+
+	/**
+	 * create a client instance.
+	 * 
+	 * @return an initialized HTTP client instance.
+	 * @throws ApixRuntimeException throw if a client instance cannot be created.
+	 */
+	public IHttpClient createClient() throws ApixRuntimeException {
+		IHttpClient httpClient = null;
+		
+		for(IHttpClientSpiProvider provider : providers) {
+			try {
+				IHttpClientSpi spi = provider.provideClientInstance();
+				
+				if(spi != null) {
+					httpClient = new HttpClientImpl(spi);
+					break;
+				}
+			} catch(Exception e) {
+				logger.info("failed to create HTTP client", e);
+			}
+		}
+		
+		if(httpClient == null)
+			throw new ApixRuntimeException("Cannot create HTTP client");
+		
+		return httpClient;
 	}
 }
 
